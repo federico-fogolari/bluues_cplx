270243a9b5a47495ee9e90875e84e84d361a3584
federico-fogolari <31154918+federico-fogolari@users.noreply.github.com>
2024-10-09 10:13:17 +0200

    Update clean_and_run.sh

--- test/clean_and_run.sh
+++ test/clean_and_run.sh
@@ -1,5 +1,5 @@
 rm -f exp* std* test_* trian*
 bluues_cplx  pdb1ay7.pqr test_ns A B -ns test_ns_aux -srf -srfpot -pa 0.1
-bluues_cplx  pdb1ay7.pqr test_msms A B -ns test_msms_aux -pa 0.1 -lite
+bluues_cplx  pdb1ay7.pqr test_msms A B -m test_msms_aux -pa 0.1 -lite
 rm -f exp* std* trian*
 
